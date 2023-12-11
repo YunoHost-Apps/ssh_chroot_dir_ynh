85e90e3e63d4a4b003e10896e9e912d3e294b317
Félix Piédallu <felix@piedallu.me>
2023-12-11 11:21:30 +0100

    Cleanup, use $data_dir

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
+user_dir="$data_dir/$ssh_user"
+
 #=================================================
 # PACKAGE CHECK BYPASSING...
 #=================================================
 
 IS_PACKAGE_CHECK () {
-	if [ ${PACKAGE_CHECK_EXEC:-0} -eq 1 ]
-	then
-		return 0
-	else
-		return 1
-	fi
+    if [ "${PACKAGE_CHECK_EXEC:-0}" -eq 1 ]; then
+        return 0
+    else
+        return 1
+    fi
 }
 
 #=================================================
